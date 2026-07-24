631e285b8dbea27b36e46ddc1fedb0dcbd3a36d4
Camila Sanches <camilasanches.clrs@gmail.com>
2026-07-23 22:49:05 -0300

    feat: implementa service de tarefas

---
+++ src/services/tarefas.service.ts
@@ -0,0 +1,43 @@
+
+import { Tarefa } from "../interfaces/tarefas.interface";
+
+const tarefas: Tarefa[] = [];
+
+export function createTarefa(title: string): Tarefa {
+    const id = Math.random();
+    const tarefa: Tarefa = { id, title, completed: false };
+    tarefas.push(tarefa);
+    return tarefa;
+}
+
+export function listTarefas(): Tarefa[] {
+    return tarefas;
+}
+
+export function getTarefaById(id: number): Tarefa | undefined {
+    return tarefas.find((tarefa) => tarefa.id === id);
+}
+
+export function updateTarefa(id: number, dados: { title?: string; completed?: boolean }): Tarefa | undefined {
+    const tarefa = getTarefaById(id);
+    if (tarefa) {
+        if (dados.title !== undefined) {
+            tarefa.title = dados.title;
+        }
+        if (dados.completed !== undefined) {
+            tarefa.completed = dados.completed;
+        }
+    }
+    return tarefa;
+}
+
+export function deleteTarefa(id: number): boolean {
+    const index = tarefas.findIndex((tarefa) => tarefa.id === id);
+
+    if (index !== -1) {
+        tarefas.splice(index, 1);
+        return true;
+    }
+
+    return false;
+}
